942fdbe813ecc213f1932438f0fadb9b1c0b19e9
Skyler Trent <skyler.trent93@gmail.com>
2022-11-16 20:21:26 -0700

    warmup challenge

---
+++ hasUniqueChars.js
@@ -0,0 +1,13 @@
+// // Write your Given a word, return true if that word contains only unique characters. Return false otherwise.
+
+// For example:
+
+// hasUniqueChars("Monday")
+// // returns true
+
+// hasUniqueChars("Moonday")
+// // returns false
+
+// Uppercase and lowercase letters should be considered separately:
+// hasUniqueChars("Bob")
+// // returns truecode below
