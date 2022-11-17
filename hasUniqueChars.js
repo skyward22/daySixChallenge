90c8938dc4ff74acf12003110e278399c3c8d271
Skyler Trent <skyler.trent93@gmail.com>
2022-11-16 20:40:26 -0700

    challenge completed

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -11,3 +11,16 @@
 // Uppercase and lowercase letters should be considered separately:
 // hasUniqueChars("Bob")
 // // returns truecode below
+
+const givenWord = (word) => {
+  for (let i = 0; i < word.length; i++) {
+    for (let x = i + 1; x < word.length; x++) {
+      if (word[i] === word[x]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+console.log(givenWord("lucyu"));
